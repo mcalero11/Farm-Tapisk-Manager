186c655e10f1fe222e16766c0d2f50b354920108
Centro de Desarrollo De Software <cds@univo.edu.sv>
2018-10-04 10:59:35 -0600

    Actualizado nugets, actualizado Android target a 8.1, Creado Master Detail, Agregado fuente de Font Awesome

--- TapiskAPP/TapiskAPP/TapiskAPP/App.xaml.cs
+++ TapiskAPP/TapiskAPP/TapiskAPP/App.xaml.cs
@@ -11,11 +11,6 @@ namespace TapiskAPP
 {
     public partial class App : PrismApplication
     {
-        /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
         public App() : this(null) { }
 
         public App(IPlatformInitializer initializer) : base(initializer) { }
@@ -24,13 +19,14 @@ namespace TapiskAPP
         {
             InitializeComponent();
 
-            await NavigationService.NavigateAsync("NavigationPage/MainPage");
+            await NavigationService.NavigateAsync($"{nameof(MasterPage)}/{nameof(NavigationPage)}/{nameof(Views.MainPage)}");
         }
 
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             containerRegistry.RegisterForNavigation<NavigationPage>();
-            containerRegistry.RegisterForNavigation<MainPage>();
+            containerRegistry.RegisterForNavigation<MainPage,MainPageViewModel>();
+            containerRegistry.RegisterForNavigation<MasterPage,MasterPageViewModel>();
         }
     }
 }
---
+++ TapiskAPP/TapiskAPP/TapiskAPP/Views/MasterPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace TapiskAPP.Views
+{
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
